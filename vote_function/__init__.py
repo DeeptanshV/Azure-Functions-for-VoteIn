d298f34f4eb744999f635b5682d96e3a7ae03c0f
Deeptansh Verma <deeptansh2996@gmail.com>
2023-07-08 22:40:25 +0530

    All Functions deployed

--- vote_function/__init__.py
+++ vote_function/__init__.py
@@ -1,24 +1,46 @@
 import logging
-
+import pathlib
+import mysql.connector 
+import json
 import azure.functions as func
+
+def get_ssl_cert():
+    current_path = pathlib.Path(__file__).parent.parent
+    print(str(current_path/ 'BaltimoreCyberTrustRoot.crt'))
+    return str(current_path/ 'BaltimoreCyberTrustRoot.crt')
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
+    
+    host = 'main-server.mysql.database.azure.com'
+    user ='Deeptansh'
+    password = "Tannu@2996"
+    port = "3306"
+    db="users"
+    
+    username=req.get_json().get("email")
+    participant=req.get_json().get("participant-id")
+    
+    try:
+        con = mysql.connector.connect( host=host,port=port,user=user,password=password,db=db,ssl_ca='C:\\Users\\deept\\Desktop\\functions\\vote\\BaltimoreCyberTrustRoot.crt.pem')
+        cursor=con.cursor()
+        query=f"update userinfo set VotedTo=%s where email=%s" 
+        print(query)
+        cursor.execute(query,(participant,username)) 
+        # data=cursor.fetchall()
+        con.commit()
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            json.dumps({
+                "Message":"Thank you, Your vote has been registered"
+            }),
+            mimetype="application/json"
+        )
+        
+        
+    except Exception as e:
+        print(e)
+        return func.HttpResponse(
+            json.dumps({"Message":"Could not register your vote"}),
+            mimetype="application/json"
         )
